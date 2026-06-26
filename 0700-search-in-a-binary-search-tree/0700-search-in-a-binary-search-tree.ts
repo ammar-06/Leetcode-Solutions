f960007dc347661e9fc3562cb934dad0c6183019
Ammar Ahmad <ammarofficial321@gmail.com>
2026-06-26 22:21:46 +0500

    Time: 0 ms (100%), Space: 63.1 MB (29.89%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.ts
@@ -0,0 +1,8 @@
+function searchBST(root: any, val: number): any {
+    while (root) {
+        if (root.val === val) return root;
+        if (val < root.val) root = root.left;
+        else root = root.right;
+    }
+    return null;
+}
