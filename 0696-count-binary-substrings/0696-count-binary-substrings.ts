34e3f70b4363125df447ac86802cc6bb1765a0df
Ammar Ahmad <ammarofficial321@gmail.com>
2026-06-26 22:20:44 +0500

    Time: 8 ms (90.7%), Space: 58.8 MB (88.37%) - LeetHub

---
+++ 0696-count-binary-substrings/0696-count-binary-substrings.ts
@@ -0,0 +1,19 @@
+function countBinarySubstrings(s: string): number {
+    let prev = 0;
+    let curr = 1;
+    let res = 0;
+
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === s[i - 1]) {
+            curr++;
+        } else {
+            res += Math.min(prev, curr);
+            prev = curr;
+            curr = 1;
+        }
+    }
+
+    res += Math.min(prev, curr);
+
+    return res;
+}
