ccbb18bfa5cf28e24a035b4c29b8c45ce877a147
Ammar Ahmad <ammarofficial321@gmail.com>
2026-07-26 14:40:22 +0500

    Time: 0 ms (100%), Space: 53.7 MB (91.37%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.ts
@@ -0,0 +1,19 @@
+function rob(nums: number[]): number {
+    const n = nums.length;
+    if (n === 1) return nums[0];
+
+    const robRange = (start: number, end: number): number => {
+        let prev1 = 0;
+        let prev2 = 0;
+
+        for (let i = start; i <= end; i++) {
+            const curr = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    };
+
+    return Math.max(robRange(0, n - 2), robRange(1, n - 1));
+}
