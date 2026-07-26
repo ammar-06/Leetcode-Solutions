b1d4e253bb4032a03ac919e0443b784a49a39dad
Ammar Ahmad <ammarofficial321@gmail.com>
2026-07-26 14:52:09 +0500

    Time: 63 ms (54.86%), Space: 65.8 MB (65.71%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.ts
@@ -0,0 +1,17 @@
+function lowestCommonAncestor(
+    root: TreeNode | null,
+    p: TreeNode | null,
+    q: TreeNode | null
+): TreeNode | null {
+    while (root) {
+        if (p!.val < root.val && q!.val < root.val) {
+            root = root.left;
+        } else if (p!.val > root.val && q!.val > root.val) {
+            root = root.right;
+        } else {
+            return root;
+        }
+    }
+
+    return null;
+}
